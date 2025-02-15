943a7db31ccf1b7739464ccf3dca317263090287
wwillcooke <willgcooke@gmail.com>
2025-02-15 14:36:24 +0000

    contact page, home page
    - fixed redirects for home page and contact page
    - added contact page functionality

--- game/home/static/home/js/home.js
+++ game/home/static/home/js/home.js
@@ -1,16 +1,1 @@
 // Author: Will Cooke
-console.log('x');
-
-window.userLoaded
-    .then((user) => { // user and their data is loaded
-
-        const loggedIn = document.getElementById("loggedIn");
-        const userIsAuthenticated = true;
-        console.log('User is authenticated:', userIsAuthenticated);
-        loggedIn.style.display = "block";
-    })
-    .catch((error) => { // user did not load
-        const loggedOut = document.getElementById("loggedOut");
-        loggedOut.style.display = "block";
-    });
-
--- game/home/views.py
+++ game/home/views.py
@@ -2,17 +2,27 @@
 
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+
 def home(request):
+    print('User:', request.user)
+    print('Username:', request.user.username)
+    print('Is Authenticated:', request.user.is_authenticated)
+
+    if not request.user.is_authenticated:
+        return redirect('')
+
     context = {
         'page_title': 'Home',
         'welcome_message': 'Welcome to the Home!',
         'description': 'Check out the latest posts and updates here.', 
         'user': {
-            'name': 'Will',
+            'name': request.user.username,
             'points': 100
         }
     }
     return render(request, 'home/home.html', context)
+
 
 #def post_detail(request, id):
 #    return render(request, 'home/post_detail.html', {'post_id': id})

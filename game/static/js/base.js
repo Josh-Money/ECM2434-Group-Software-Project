9d878d113fee6c886ea3e18bc338ce2b805fa9c4
wwillcooke <willgcooke@gmail.com>
2025-02-21 16:42:04 +0000

    logged out users are redirected to '/' using middleware

--- game/static/js/base.js
+++ game/static/js/base.js
@@ -1,1 +1,11 @@
-console.log('base loaded')
+// document.addEventListener("DOMContentLoaded", function () {
+//     var isAuthenticated = document.body.getAttribute("data-authenticated") === "true";
+//     var currentPath = window.location.pathname;
+
+//     if (!isAuthenticated && currentPath !== "/") {
+//         window.location.href = "/";
+//         console.log('not logged in, redirecting')
+//     } else {
+//         console.log('logged in.')
+//     }
+// });
